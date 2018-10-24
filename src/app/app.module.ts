8a3613a2dc4726eab24de8cd794b158cb31c952a
franksun <sun_si_yan@163.com>
2018-10-24 15:04:40 +0800

    引用element-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ElModule } from 'element-angular';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ElModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
