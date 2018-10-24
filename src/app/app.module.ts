2e9c4d437776a1a10ccdd4565a0dafd2a8045af3
franksun <sun_si_yan@163.com>
2018-10-24 16:58:11 +0800

    引入FormModule和HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
+// 引入模块
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ElModule } from 'element-angular';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // 调用组件
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    ElModule.forRoot()
+    ElModule.forRoot(),
+    FormsModule,
+    HttpClientModule
   ],
+  // 调用服务
   providers: [],
+  // 调用依赖
   bootstrap: [AppComponent]
 })
 export class AppModule { }
